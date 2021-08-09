d32fcddca9ccf88ffca82b25549e494301af4bd2
DESKTOP-N91C8ET\alekp <alekpawlowski21@gmail.com>
2021-08-09 15:20:32 +0200

    set books

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 import useDropdown from "./useDropdown";
-import * as mockData from "../mock/books.json";
 
-const Add = () => {
+const Add = ({ books, setBooks }) => {
     const [author, setAuthor] = useState("");
-    // const [genre, setGenre] = useState("");
-    // const [audience, setAudience] = useState("");
+    const [title, setTitle] = useState("");
+    const [publicationDate, setPublicationDate] = useState("");
     const genres = ["kryminal", "fantastyka", "horror"];
     const audiences = ["Dzieci", "Młodzież", "Dorośli"];
     const [genre, GenreDropdown] = useDropdown("", "Gatunek", genres);
     const [audience, AudienceDropdown] = useDropdown("", "Dla", audiences);
 
+    const addBook = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        const newBook = {
+            author: author,
+            title: title,
+            publicationDate: publicationDate,
+            genre: genre,
+            audience: audience,
+        };
+        const newBooks = [...books, newBook];
+
+        setBooks(newBooks);
+    };
+
     return (
         <div>
             <form>
+                <label htmlFor="title">
+                    Tytuł
+                    <input
+                        id="title"
+                        value={title}
+                        placeholder="wpisz tytuł"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                </label>
+                <br />
                 <label htmlFor="author">
                     Autor
                     <input
@@ -24,11 +48,23 @@ const Add = () => {
                     />
                 </label>
                 <br />
+                <label htmlFor="publicationDate">
+                    data publikacji
+                    <input
+                        id="publicationDate"
+                        value={publicationDate}
+                        placeholder="wpisz date"
+                        onChange={(event) =>
+                            setPublicationDate(event.target.value)
+                        }
+                    />
+                </label>
+                <br />
                 <GenreDropdown />
                 <br></br>
                 <AudienceDropdown />
                 <br />
-                <button>Dodaj</button>
+                <button onClick={(event) => addBook(event)}>Dodaj</button>
             </form>
         </div>
     );
