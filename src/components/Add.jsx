f009b4fdcdc52e0491df98cb5a37c120012d2f60
DESKTOP-N91C8ET\alekp <alekpawlowski21@gmail.com>
2021-08-09 14:16:05 +0200

    mock data

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from "react";
 import useDropdown from "./useDropdown";
+import * as mockData from "../mock/books.json";
 
 const Add = () => {
     const [author, setAuthor] = useState("");
     // const [genre, setGenre] = useState("");
     // const [audience, setAudience] = useState("");
     const genres = ["kryminal", "fantastyka", "horror"];
-    const audiences = ["dzieci", "mlodziez", "dorosli"];
+    const audiences = ["Dzieci", "Młodzież", "Dorośli"];
     const [genre, GenreDropdown] = useDropdown("", "Gatunek", genres);
     const [audience, AudienceDropdown] = useDropdown("", "Dla", audiences);
+    const [books, setBooks] = useState([]);
 
     useEffect(() => {
-        console.log("useEffect");
-    }, [genre]);
+        fetch("https://clockworkjava.pl/books.php")
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setBooks(data);
+                console.log("data", data);
+            });
+        // setBooks(mockData);
+    }, []);
 
     return (
         <div>
